6e9a2ceb9cd88008695135f3a2b0d053b3987a4c
ArchDud3 <albrechtarcher@gmail.com>
2023-02-04 06:23:19 +1100

    Los a files

---
+++ rusty-main/target/debug/deps/rusty_main-4e9185a80538f230.d
@@ -0,0 +1,5 @@
+/Users/archeralbrecht/Documents/GitHub/RUSTY/rusty-main/target/debug/deps/rusty_main-4e9185a80538f230: src/main.rs
+
+/Users/archeralbrecht/Documents/GitHub/RUSTY/rusty-main/target/debug/deps/rusty_main-4e9185a80538f230.d: src/main.rs
+
+src/main.rs:
---
+++ rusty-main/target/debug/rusty-main.d
@@ -0,0 +1,1 @@
+/Users/archeralbrecht/Documents/GitHub/RUSTY/rusty-main/target/debug/rusty-main: /Users/archeralbrecht/Documents/GitHub/RUSTY/rusty-main/src/main.rs
